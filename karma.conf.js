2a66eaaebe984f07ac39d77d2be43a0fba915a55
Guest User <Guest@Epicodus-4C.local>
2018-01-05 09:57:55 -0800

    ensure that css is being concatenated

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jquery-3.2.1','jasmine','browserify'],
 
 
     // list of files / patterns to load in the browser
@@ -26,6 +26,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'js/*.js': [ 'browserify' ],
+      'spec/*.js': [ 'browserify' ],
+    },
+
+    plugins: [
+      'karma-jquery',
+      'karma-browserify',
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-jasmine-html-reporter'
+    ],
+
+    browserify: {
+      debug: true,
+      transform: [ [ 'babelify', {presets: ["es2015"]} ] ]
     },
 
 
