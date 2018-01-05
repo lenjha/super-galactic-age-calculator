6421c212e4c092d2652a82a5b2aa588a6b8f39fa
Guest User <Guest@Epicodus-4C.local>
2018-01-05 09:05:23 -0800

    add bower files

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,17 @@ var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
 var buildProduction = utilities.env.production;
-
+var lib = require('bower-files')({
+  'overrides':{
+    'bootstrap':{
+      'main':[
+        'less/bootstrap.less',
+        'dist/css/bootstrap.css',
+        'dist/js/bootstrap.js'
+      ]
+    }
+  }
+});
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
@@ -39,4 +49,26 @@ gulp.task('bowerJS', function() {
     .pipe(concat('vendor.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function() {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('clean', function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task('build', ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+  gulp.start('bower');
+  gulp.start('cssBuild');
 });
