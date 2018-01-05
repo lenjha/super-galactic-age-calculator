7496e490d261ad83b6209faad8ad8a7bc99d6883
Guest User <Guest@Epicodus-4C.local>
2018-01-05 09:17:32 -0800

    add css and crowser-syncing

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,8 @@ var lib = require('bower-files')({
     }
   }
 });
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
@@ -71,4 +73,36 @@ gulp.task('build', ['clean'], function(){
   }
   gulp.start('bower');
   gulp.start('cssBuild');
+});
+
+gulp.task('serve', ['build'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(["scss/*.scss", "scss/**/*.scss"], ['cssBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+gulp.task('htmlBuild', function(){
+  browserSync.reload();
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src('./scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
