fae4758f87f977fb383e102d68b8676f3d0109c1
Guest User <Guest@Epicodus-4C.local>
2018-01-05 08:55:35 -0800

    add linter jshint

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,29 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var jshint = require('gulp-jshint');
+var buildProduction = utilities.env.production;
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./tmp/allConcat.js']})
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(gulp.dest('./build/js'));
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
 });
 
 gulp.task('concatInterface', function(){
   return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./tmp/allConcat.js']})
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('minifyScripts', ['jsBrowserify'], function() {
