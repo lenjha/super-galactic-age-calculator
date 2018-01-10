60392caf69112f63fe21f88a164d3342e20164ca
Guest User <Guest@Epicodus-4D.local>
2018-01-09 19:54:11 -0800

    fix syntax of test-spec.js

---
+++ README.md
@@ -0,0 +1,21 @@
+# Super Galactic Age Calculator Ultra Mega
+
+####
+####
+
+## Description
+####
+
+## Setup
+
+$npm install
+$bower install
+$npm build
+$npm serve
+
+## Technologies
+
+## Plans for the Future
+
+## License
+Copyright (c) 2018, Lena Ha
--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,8 +1,7 @@
 import { User } from './../js/scripts.js';
 
 describe('User', function(){
-  let user = new User("user");
-  user.age = 10;
+  let user = new User(10);
 
   // beforeEach(function(){
   // });
@@ -10,8 +9,14 @@ describe('User', function(){
   it('should accept an integer', function(){
     expect(user.age).toEqual(10);
   });
+
   it('should calculate age on Mercury in seconds', function(){
     user.calcAgeMercury();
     expect(user.ageMercurySeconds).toEqual(75686400);
+  });
+
+  it('should calculate age on Mercury from the seconds', function(){
+    user.calcAgeMercury();
+    expect(user.ageMercury).toEqual(2.4);
   });
 });
